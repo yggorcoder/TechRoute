2de6484e374b2466395559746bfca8c762044b95
yggorcoder <yggor.ramos@gmail.com>
2025-10-16 20:08:42 -0300

    fix: add absolute path to sys.path for module resolution

--- server/main.py
+++ server/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routers import router as visit_router
